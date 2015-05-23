b709172c40b043c9f8f2bad56a9aa3b853aca595
mrtnvm <mrtnv.m@gmail.com>
2015-05-23 16:18:39 +0300

    Добавлены изображения, bootstrap, font-awesome

---
+++ ReadTheNews/Controllers/RssNewsController.cs
@@ -0,0 +1,35 @@
+﻿using ReadTheNews.Helpers;
+using ReadTheNews.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web.Mvc;
+
+namespace ReadTheNews.Controllers
+{
+    [Authorize]
+    public class RssNewsController : Controller
+    {
+        private RssNewsContext db = new RssNewsContext();
+
+        public ActionResult Index()
+        {
+            return Redirect("RssNews/Channels");
+        }
+
+        [AllowAnonymous]
+        public ActionResult Channels()
+        {
+            try
+            {
+                ViewBag.RssChannels = db.RssChannels.ToList();
+            }
+            catch (Exception ex)
+            {
+                TempData["Error"] = ex.Message;
+                return RedirectToRoute(new { controller = "RssNews", action = "Error" });
+            }
+            return View();
+        }
+    }
+}
