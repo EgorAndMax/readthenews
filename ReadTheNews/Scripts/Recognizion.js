5a568f7410fc7f9f5b0a19ea21c0356f4074473f
unknown <lukin.net@gmail.com>
2015-06-01 18:32:59 +0300

    Исправлена ошибка с переадресацией

--- ReadTheNews/Scripts/Recognizion.js
+++ ReadTheNews/Scripts/Recognizion.js
@@ -16,6 +16,70 @@
             msg.text = 'привет любитель новостей';
             speechSynthesis.speak(msg);
         },
+        /*
+        'вкладка *term': function(term)
+        {
+            if (term == 'прочитать позже')
+            {
+                location.replace("http://localhost:9710/RssNews/ReadItLater");
+                msg.text = 'вкладка прочитать позже';
+                speechSynthesis.speak(msg);
+            }
+            else if (term == 'избранные новости')
+            {
+                location.replace("http://localhost:9710/RssNews/MyFavoriteNews");
+                msg.text = 'вкладка избранные новости';
+                speechSynthesis.speak(msg);
+            }
+            else if (term == 'мои новости')
+            {
+                location.replace("http://localhost:9710/RssNews/MyNews");
+                msg.text = 'вкладка мои новости';
+                speechSynthesis.speak(msg);
+            }
+            else if (term == 'мои каналы')
+            {
+                location.replace("http://localhost:9710/RssNews/MyChannels");
+                msg.text = 'вкладка мои каналы';
+                speechSynthesis.speak(msg);
+            }
+            else if (term == 'каналы')
+            {
+                location.replace("http://localhost:9710/RssNews/Channels");
+                msg.text = 'вкладка каналы';
+                speechSynthesis.speak(msg);
+            }
+            else if (term == 'отчеты')
+            {
+                location.replace("http://localhost:9710/Reports/Index");
+                msg.text = 'вкладка отчеты';
+                speechSynthesis.speak(msg);
+            }
+
+        },
+        */
+        'категория :name': function (name) {
+            if (name == 'мир' || name == 'спорт' || name == 'бывший ссср' || name == 'россия' || name == 'силовые структуры' || name == 'новости' || name == 'из жизни' || name == 'культура' || name == 'приложения') {
+                location.replace("http://localhost:9710/RssNews/GetNewsByCategory?Name=" + name);
+                msg.text = 'категория ' + name;
+                speechSynthesis.speak(msg);
+            }
+            else {
+                msg.text = 'категория не найдена';
+                speechSynthesis.speak(msg);
+            }
+        },
+        'обзорщик *command': function (command) {
+            if (command == 'как дела') {
+                msg.text = 'всё отлично';
+                speechSynthesis.speak(msg);
+            }
+            else if (command == 'в чем смысл жизни') {
+                msg.text = 'сорок два';
+                speechSynthesis.speak(msg);
+            }
+        },
+        
         'прочитать позже': function () {
             location.replace("http://localhost:9710/RssNews/ReadItLater");
             msg.text = 'вкладка прочитать позже';
@@ -40,12 +104,8 @@
             location.replace("http://localhost:9710/Reports/Index");
             msg.text = 'вкладка отчеты';
             speechSynthesis.speak(msg);
-        },
-        'категория :name': function (name) {
-            location.replace("http://localhost:9710/Reports/GetNewsByCategory?Name=" + name);
-            msg.text = 'вкладка' + ':name';
-            speechSynthesis.speak(msg);
         }
+        
     };
 
     annyang.addCommands(commands);
